ce1b9715ea9c7202b084de31ca3ae198f64d3a19
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2022-10-24 19:22:44 +0300

    Add files via upload

---
+++ three_exercise/three_exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите год: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите номер месяца: ");
+            int mounth = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите день: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Дата предыдущего дня: {day - 1}.{mounth}.{year}");
+            Console.WriteLine($"Дата следующего дня: {day + 1}.{mounth}.{year}");
+
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            {
+                Console.WriteLine("Год високосный");
+            }
+            else
+            {
+                Console.WriteLine("Год не високосный");
+            }
+
+
+        }
+    }
